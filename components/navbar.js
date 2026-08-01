a1e2fb957101177807e2691593390899eb1adf2a
MUHAMMAD-SAAD746 <>
2026-08-01 18:33:23 +0500

    navbar updated for authenticated user, trade page created

--- components/navbar.js
+++ components/navbar.js
@@ -9,11 +9,10 @@ function loadNavbar() {
                     <img class="desktop-logo" src="./assets/images/logo.PNG" alt="Logo">
                     <img class="mobile-logo" src="./assets/images/charcoal-logo.png" alt="Logo">
                 </div>
-                
+
 
                 <!-- Toggle Button -->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
-
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -22,27 +21,27 @@ function loadNavbar() {
 
                     <div class="mobile-nav-links">
                         <ul class="nav-list mx-auto">
-                            <li class="${currentPage === "index.html" ? "active" : ""}">
+                            <li class="${currentPage === "index.html" ? "active" : "" }">
                                 <ion-icon name="home-outline"></ion-icon>
                                 <a href="./index.html">Home</a>
                             </li>
 
-                            <li class="${currentPage === "market.html" ? "active" : ""}">
+                            <li class="${currentPage === "market.html" ? "active" : "" }">
                                 <ion-icon name="flash-outline"></ion-icon>
                                 <a href="./market.html">Market</a>
                             </li>
 
-                            <li class="${currentPage === "watchlist.html" ? "active" : ""}">
+                            <li class="${currentPage === "watchlist.html" ? "active" : "" }">
                                 <ion-icon name="bookmark-outline"></ion-icon>
                                 <a href="#">Watchlist</a>
                             </li>
 
-                            <li class="${currentPage === "portfolio.html" ? "active" : ""}">
+                            <li class="${currentPage === "portfolio.html" ? "active" : "" }">
                                 <ion-icon name="briefcase-outline"></ion-icon>
                                 <a href="#">Portfolio</a>
                             </li>
 
-                            <li class="${currentPage === "learn.html" ? "active" : ""}">
+                            <li class="${currentPage === "learn.html" ? "active" : "" }">
                                 <ion-icon name="book-outline"></ion-icon>
                                 <a href="#">Learn</a>
                             </li>
@@ -59,6 +58,37 @@ function loadNavbar() {
                                     <a class="dropdown-item" href="#">
                                         Select Currency
                                     </a>
+                                </li>
+                            </ul>
+                        </div>
+
+
+                        <div class="auth-links">
+                            <button class="auth-nav-btn">
+                                <ion-icon name="flash-outline"></ion-icon>
+                                <span>Trade History</span>
+                            </button>
+
+                            <button class="auth-nav-btn">
+                                <ion-icon name="wallet-outline"></ion-icon>
+                                <span>Wallet</span>
+                            </button>
+                        </div>
+
+
+                        <div class="dropdown profile-dropdown">
+                            <button class="btn profile-btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <img src="./assets/images/profile male.png" alt="Profile">
+                                <span>Saad</span>
+                            </button>
+
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li>
+                                    <button class="dropdown-item" type="button">
+                                        <ion-icon name="log-out-outline"></ion-icon>
+                                        Logout
+                                    </button>
                                 </li>
                             </ul>
                         </div>
