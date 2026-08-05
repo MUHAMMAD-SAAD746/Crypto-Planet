661fff10ea27fffea190c57d33b02dcb589ea695
MUHAMMAD-SAAD746 <>
2026-08-05 15:30:51 +0500

    footer updated for authenticated user

--- components/footer.js
+++ components/footer.js
@@ -1,184 +1,213 @@
 function loadFooter() {
     return `
         <footer>
-        <div class="container-fluid">
-            <div class="row g-1">
-                <div class="col-12 col-lg-4 mb-4 mb-lg-0">
-                    <div id="footer-container" class="container-fluid px-0">
-                        <img src="./assets/images/charcoal-logo.PNG" alt="">
+            <div class="container-fluid">
+                <div class="row g-1">
+                    <div class="col-12 col-lg-4 mb-4 mb-lg-0">
+                        <div id="footer-container" class="container-fluid px-0">
+                            <img src="./assets/images/charcoal-logo.PNG" alt="">
 
-                        <p class="pe-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis minima est harum molestias.
-                            Minus
-                            qui placeat nam inventore nulla voluptatem.</p>
+                            <p class="pe-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis minima est harum
+                                molestias.
+                                Minus
+                                qui placeat nam inventore nulla voluptatem.</p>
 
-                        <div class="row desktop-icons">
-                            <div class="col col-md-2"><ion-icon name="logo-facebook"></ion-icon></div>
-                            <div class="col col-md-2"><ion-icon name="logo-instagram"></ion-icon></div>
-                            <div class="col col-md-2"><ion-icon name="logo-youtube"></ion-icon></div>
-                            <div class="col col-md-2"><ion-icon name="logo-twitter"></ion-icon></div>
-                            <div class="col col-md-2"><ion-icon name="logo-linkedin"></ion-icon></div>
+                            <div class="row desktop-icons">
+                                <div class="col col-md-2"><ion-icon name="logo-facebook"></ion-icon></div>
+                                <div class="col col-md-2"><ion-icon name="logo-instagram"></ion-icon></div>
+                                <div class="col col-md-2"><ion-icon name="logo-youtube"></ion-icon></div>
+                                <div class="col col-md-2"><ion-icon name="logo-twitter"></ion-icon></div>
+                                <div class="col col-md-2"><ion-icon name="logo-linkedin"></ion-icon></div>
+                            </div>
+                            <div class="row mobile-icons">
+                                <div class="col col-md-2">
+                                    <ion-icon name="logo-windows"></ion-icon>
+                                    <div>
+                                        <span>Download PC-Client</span>
+                                        <span>Windows</span>
+                                    </div>
+                                </div>
+                                <div class="col col-md-2">
+                                    <ion-icon name="logo-apple"></ion-icon>
+                                    <div>
+                                        <span>Download for the</span>
+                                        <span>MacOS</span>
+                                    </div>
+                                </div>
+                                <div class="col col-md-2">
+                                    <ion-icon name="logo-google-playstore"></ion-icon>
+                                    <div>
+                                        <span>Download on the</span>
+                                        <span>App Store</span>
+                                    </div>
+                                </div>
+                                <div class="col col-md-2">
+                                    <ion-icon name="logo-apple-appstore"></ion-icon>
+                                    <div>
+                                        <span>Get it on</span>
+                                        <span>Google Play</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="row mobile-icons">
-                            <div class="col col-md-2"><ion-icon name="logo-windows"></ion-icon></div>
-                            <div class="col col-md-2"><ion-icon name="logo-apple"></ion-icon></div>
-                            <div class="col col-md-2"><ion-icon name="logo-google-playstore"></ion-icon></div>
-                            <div class="col col-md-2"><ion-icon name="logo-apple-appstore"></ion-icon></div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg">
+                        <div>
+                            <h4>Exchange</h4>
+
+                            <ul>
+                                <li><a href="#">Exchange Home</a></li>
+                                <li><a href="#">Margin Trading</a></li>
+                                <li><a href="#">Derivatives Trading</a></li>
+                                <li><a href="#">Supercharger</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg">
+                        <div>
+                            <h4>Support</h4>
+
+                            <ul>
+                                <li><a href="#">Request Form</a></li>
+                                <li><a href="#">Contact Support</a></li>
+                                <li><a href="#">FAQ</a></li>
+                                <li><a href="#">Security</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg">
+                        <div>
+                            <h4>Resources</h4>
+
+                            <ul>
+                                <li><a href="#">Downloads</a></li>
+                                <li><a href="#">Desktop Application</a></li>
+                                <li><a href="#">Buy Crypto</a></li>
+                                <li><a href="#">Refferal</a></li>
+                                <li><a href="#">Listing Trading</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg learn-column">
+                        <div>
+                            <h4>Learn</h4>
+
+                            <ul>
+                                <li><a href="#">What's Trending</a></li>
+                                <li><a href="#">Product News</a></li>
+                                <li><a href="#">Events</a></li>
+                                <li><a href="#">University</a></li>
+                                <li><a href="#">Research</a></li>
+                                <li><a href="#">Market Update</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg">
+                        <div>
+                            <h4>Company</h4>
+
+                            <ul>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Careers</a></li>
+                                <li><a href="#">News</a></li>
+                                <li><a href="#">Security</a></li>
+                                <li><a href="#">Community</a></li>
+                                <li><a href="#">Announcements</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+                    <div class="col-12 col-md-12 col-lg newsletter-column">
+                        <div class="container-fluid newsletter-section p-0">
+                            <h4>Newsletter</h4>
+
+                            <div class="newsletter-form">
+                                <input type="email" name="" id="" placeholder="Enter your email">
+                                <button>Send</button>
+                            </div>
+
+                            <p class="text-center">We accept following payment systems</p>
+
+                            <div class="payment-methods">
+                                <div class="payment-method">
+                                    <ion-icon name="logo-paypal"></ion-icon>
+                                </div>
+
+                                <div class="payment-method">
+                                    <ion-icon name="card-outline"></ion-icon>
+                                </div>
+
+                                <div class="payment-method">
+                                    <ion-icon name="logo-bitcoin"></ion-icon>
+                                </div>
+
+                                <div class="payment-method">
+                                    <ion-icon name="id-card-outline"></ion-icon>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-6 col-md-4 col-lg">
-                    <div>
-                        <h4>Exchange</h4>
 
-                        <ul>
-                            <li><a href="#">Exchange Home</a></li>
-                            <li><a href="#">Margin Trading</a></li>
-                            <li><a href="#">Derivatives Trading</a></li>
-                            <li><a href="#">Supercharger</a></li>
-                        </ul>
+                <hr>
+            </div>
+
+
+
+            <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center gap-4 pt-4"
+                id="copyright">
+                <div class="copyright-text">
+                    <p>© 2022 dynamic. All rights reserved</p>
+
+                    <div class="copyright-links">
+                        <a href="">Privacy</a>
+                        <span>|</span>
+                        <a href="">Terms</a>
+                        <span>|</span>
+                        <a href="">Sitemap</a>
                     </div>
                 </div>
-                <div class="col-6 col-md-4 col-lg">
-                    <div>
-                        <h4>Support</h4>
 
-                        <ul>
-                            <li><a href="#">Request Form</a></li>
-                            <li><a href="#">Contact Support</a></li>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">Security</a></li>
-                        </ul>
-                    </div>
+                <div class="row social-icons">
+                    <div class="col col-md-2"><ion-icon name="logo-facebook"></ion-icon></div>
+                    <div class="col col-md-2"><ion-icon name="logo-instagram"></ion-icon></div>
+                    <div class="col col-md-2"><ion-icon name="logo-youtube"></ion-icon></div>
+                    <div class="col col-md-2"><ion-icon name="logo-twitter"></ion-icon></div>
+                    <div class="col col-md-2"><ion-icon name="logo-linkedin"></ion-icon></div>
                 </div>
-                <div class="col-6 col-md-4 col-lg">
-                    <div>
-                        <h4>Resources</h4>
 
-                        <ul>
-                            <li><a href="#">Downloads</a></li>
-                            <li><a href="#">Desktop Application</a></li>
-                            <li><a href="#">Buy Crypto</a></li>
-                            <li><a href="#">Refferal</a></li>
-                            <li><a href="#">Listing Trading</a></li>
-                        </ul>
+                <div class="d-flex flex-wrap justify-content-center gap-3">
+                    <div class="footer-os">
+                        <ion-icon class="fs-4 me-1" name="logo-windows"></ion-icon>
+                        <div>
+                            <p class="text-light">Download PC-Client</p>
+                            <p class="text-light">Windows</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-6 col-md-4 col-lg learn-column">
-                    <div>
-                        <h4>Learn</h4>
-
-                        <ul>
-                            <li><a href="#">What's Trending</a></li>
-                            <li><a href="#">Product News</a></li>
-                            <li><a href="#">Events</a></li>
-                            <li><a href="#">University</a></li>
-                            <li><a href="#">Research</a></li>
-                            <li><a href="#">Market Update</a></li>
-                        </ul>
+                    <div class="footer-os">
+                        <ion-icon class="fs-4 me-1" name="logo-apple"></ion-icon>
+                        <div>
+                            <p class="text-light">Download PC-Client</p>
+                            <p class="text-light">Windows</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-6 col-md-4 col-lg">
-                    <div>
-                        <h4>Company</h4>
-
-                        <ul>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">News</a></li>
-                            <li><a href="#">Security</a></li>
-                            <li><a href="#">Community</a></li>
-                            <li><a href="#">Announcements</a></li>
-                        </ul>
+                    <div class="footer-os">
+                        <ion-icon class="fs-4 me-1" name="logo-apple-appstore"></ion-icon>
+                        <div>
+                            <p class="text-light">Download PC-Client</p>
+                            <p class="text-light">Windows</p>
+                        </div>
+                    </div>
+                    <div class="footer-os">
+                        <ion-icon class="fs-4 me-1" name="logo-google-playstore"></ion-icon>
+                        <div>
+                            <p class="text-light">Download PC-Client</p>
+                            <p class="text-light">Windows</p>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="container-fluid newsletter-section p-0">
-                <h4>Newsletter</h4>
-
-                <div class="newsletter-form">
-                    <input type="email" name="" id="" placeholder="Enter your email">
-                    <button>Send</button>
-                </div>
-
-                <p class="text-center">We accept following payment systems</p>
-
-                <div class="payment-methods">
-                    <div class="payment-method">
-                        <ion-icon name="logo-paypal"></ion-icon>
-                    </div>
-
-                    <div class="payment-method">
-                        <ion-icon name="card-outline"></ion-icon>
-                    </div>
-
-                    <div class="payment-method">
-                        <ion-icon name="logo-bitcoin"></ion-icon>
-                    </div>
-
-                    <div class="payment-method">
-                        <ion-icon name="id-card-outline"></ion-icon>
-                    </div>
-                </div>
-            </div>
-            <hr>
-        </div>
-
-
-
-        <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center gap-4 pt-4"
-            id="copyright">
-            <div class="copyright-text">
-                <p>© 2022 dynamic. All rights reserved</p>
-
-                <div class="copyright-links">
-                    <a href="">Privacy</a>
-                    <span>|</span>
-                    <a href="">Terms</a>
-                    <span>|</span>
-                    <a href="">Sitemap</a>
-                </div>
-            </div>
-
-            <div class="row social-icons">
-                <div class="col col-md-2"><ion-icon name="logo-facebook"></ion-icon></div>
-                <div class="col col-md-2"><ion-icon name="logo-instagram"></ion-icon></div>
-                <div class="col col-md-2"><ion-icon name="logo-youtube"></ion-icon></div>
-                <div class="col col-md-2"><ion-icon name="logo-twitter"></ion-icon></div>
-                <div class="col col-md-2"><ion-icon name="logo-linkedin"></ion-icon></div>
-            </div>
-
-            <div class="d-flex flex-wrap justify-content-center gap-3">
-                <div class="footer-os">
-                    <ion-icon class="fs-4 me-1" name="logo-windows"></ion-icon>
-                    <div>
-                        <p class="text-light">Download PC-Client</p>
-                        <p class="text-light">Windows</p>
-                    </div>
-                </div>
-                <div class="footer-os">
-                    <ion-icon class="fs-4 me-1" name="logo-apple"></ion-icon>
-                    <div>
-                        <p class="text-light">Download PC-Client</p>
-                        <p class="text-light">Windows</p>
-                    </div>
-                </div>
-                <div class="footer-os">
-                    <ion-icon class="fs-4 me-1" name="logo-apple-appstore"></ion-icon>
-                    <div>
-                        <p class="text-light">Download PC-Client</p>
-                        <p class="text-light">Windows</p>
-                    </div>
-                </div>
-                <div class="footer-os">
-                    <ion-icon class="fs-4 me-1" name="logo-google-playstore"></ion-icon>
-                    <div>
-                        <p class="text-light">Download PC-Client</p>
-                        <p class="text-light">Windows</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+        </footer>
     `
 }
